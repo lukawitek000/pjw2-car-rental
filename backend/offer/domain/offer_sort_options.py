55066f9017551cfed147a6b271e014bc3d18d982
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2024-06-01 19:35:24 +0200

    Reformat

--- backend/offer/domain/offer_sort_options.py
+++ backend/offer/domain/offer_sort_options.py
@@ -5,6 +5,7 @@ class OfferSortOptions:
     def __init__(self, sort_by_price=None):
         self.sort_by_price = SortByPrice(sort_by_price) if sort_by_price else None
 
+
 class SortByPrice(Enum):
     FROM_LOWEST = 'from_lowest'
     FROM_HIGHEST = 'from_highest'
