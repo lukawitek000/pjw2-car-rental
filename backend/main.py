4f40596bcbfde57fc8bcb1947a23967924e2ff55
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2024-06-01 17:46:03 +0200

    Enable cors for angular 4200 port

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+
 from authentication.auth import set_up_auth
 from di import init_di
 from database import set_up_database
 from offer.owner_operations import set_up_owner_operations
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
+
 
 if __name__ == '__main__':  # Running the flask app
     set_up_database()
