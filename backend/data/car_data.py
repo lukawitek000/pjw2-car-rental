b2363dbc55f5ff4be45ce720c59c21a79588e959
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2024-06-02 12:38:50 +0200

    Add more cars

--- backend/data/car_data.py
+++ backend/data/car_data.py
@@ -16,6 +16,7 @@ audi_models = [
     CarModel("TT"),
     CarModel("e-tron"),
 ]
+audi = CarManufacturer("Audi", audi_models)
 
 bmw_models = [
     CarModel("1 Series"),
@@ -37,8 +38,82 @@ bmw_models = [
     CarModel("i3"),
     CarModel("i8"),
 ]
-
-audi = CarManufacturer("Audi", audi_models)
 bmw = CarManufacturer("BMW", bmw_models)
 
-car_manufacturers = [audi, bmw]
+tesla_models = [
+    CarModel("Model S"),
+    CarModel("Model 3"),
+    CarModel("Model X"),
+    CarModel("Model Y"),
+]
+tesla = CarManufacturer("Tesla", tesla_models)
+
+toyota_models = [
+    CarModel("Corolla"),
+    CarModel("Camry"),
+    CarModel("Prius"),
+]
+toyota = CarManufacturer("Toyota", toyota_models)
+
+mercedes_models = [
+    CarModel("A-Class"),
+    CarModel("B-Class"),
+    CarModel("C-Class"),
+    CarModel("E-Class"),
+    CarModel("S-Class"),
+]
+mercedes = CarManufacturer("Mercedes", mercedes_models)
+
+seat_models = [
+    CarModel("Ibiza"),
+    CarModel("Leon"),
+    CarModel("Arona"),
+    CarModel("Ateca"),
+]
+seat = CarManufacturer("Seat", seat_models)
+
+renault_models = [
+    CarModel("Clio"),
+    CarModel("Megane"),
+    CarModel("Captur"),
+    CarModel("Kadjar"),
+]
+renault = CarManufacturer("Renault", renault_models)
+
+ford_models = [
+    CarModel("Fiesta"),
+    CarModel("Focus"),
+    CarModel("Mustang"),
+    CarModel("Explorer"),
+    CarModel("F-150"),
+]
+ford = CarManufacturer("Ford", ford_models)
+
+peugeot_models = [
+    CarModel("208"),
+    CarModel("308"),
+    CarModel("508"),
+    CarModel("2008"),
+    CarModel("3008"),
+]
+peugeot = CarManufacturer("Peugeot", peugeot_models)
+
+citroen_models = [
+    CarModel("C1"),
+    CarModel("C3"),
+    CarModel("C4"),
+    CarModel("C5"),
+    CarModel("Berlingo"),
+]
+citroen = CarManufacturer("Citroën", citroen_models)
+
+fiat_models = [
+    CarModel("500"),
+    CarModel("Panda"),
+    CarModel("Tipo"),
+    CarModel("Punto"),
+    CarModel("Ducato"),
+]
+fiat = CarManufacturer("Fiat", fiat_models)
+
+car_manufacturers = [audi, bmw, tesla, toyota, mercedes, seat, renault, ford, peugeot, citroen, fiat]
