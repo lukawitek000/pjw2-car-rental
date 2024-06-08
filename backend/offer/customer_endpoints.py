998a277e3326739e2aedf4042591ff69a2765603
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2024-06-08 14:33:08 +0200

    Change json body to path parameters for get_all_offers endpoint

--- backend/offer/customer_endpoints.py
+++ backend/offer/customer_endpoints.py
@@ -15,29 +15,19 @@ def set_up_customer_endpoints(app):
     app.register_blueprint(customer_endpoints)
 
 
-@customer_endpoints.route("/get_all_offers", methods=['GET'])
-def get_offers(offer_service: OfferService):
-    params_json = request.get_json() or {}
+@customer_endpoints.route("/get_all_offers", methods=['GET'], defaults={'start_date_time': "2023-01-01T00:00:00", 'end_date_time': "2023-03-31T00:00:00", 'pickup_location': None, 'return_location': None, 'sort_by_price': None})
+@customer_endpoints.route("/get_all_offers/<string:start_date_time>", methods=['GET'], defaults={'end_date_time': "2023-03-31T00:00:00", 'pickup_location': None, 'return_location': None, 'sort_by_price': None})
+@customer_endpoints.route("/get_all_offers/<string:start_date_time>/<string:end_date_time>", methods=['GET'], defaults={'pickup_location': None, 'return_location': None, 'sort_by_price': None})
+@customer_endpoints.route("/get_all_offers/<string:start_date_time>/<string:end_date_time>/<string:pickup_location>", methods=['GET'], defaults={'return_location': None, 'sort_by_price': None})
+@customer_endpoints.route("/get_all_offers/<string:start_date_time>/<string:end_date_time>/<string:pickup_location>/<string:return_location>", methods=['GET'], defaults={'sort_by_price': None})
+@customer_endpoints.route("/get_all_offers/<string:start_date_time>/<string:end_date_time>/<string:pickup_location>/<string:return_location>/<string:sort_by_price>", methods=['GET'])
+def get_offers(offer_service: OfferService, start_date_time="2023-01-01T00:00:00", end_date_time="2023-03-31T00:00:00", pickup_location=None, return_location=None, sort_by_price=None):
+    start_date_time = datetime.strptime(start_date_time, '%Y-%m-%dT%H:%M:%S')
+    end_date_time = datetime.strptime(end_date_time, '%Y-%m-%dT%H:%M:%S')
 
-    default_filter_params = {
-        'start_date_time': "2023-01-01T00:00:00",
-        'end_date_time': "2023-03-31T00:00:00",
-        'pickup_location': None,
-        'return_location': None
-    }
-
-    default_sort_params = {
-        'sort_by_price': None
-    }
-
-    filter_params_json = params_json.get('filter', default_filter_params)
-    filter_params_json['start_date_time'] = datetime.strptime(filter_params_json['start_date_time'], '%Y-%m-%dT%H:%M:%S')
-    filter_params_json['end_date_time'] = datetime.strptime(filter_params_json['end_date_time'], '%Y-%m-%dT%H:%M:%S')
-
-    sort_params_json = params_json.get('sort', default_sort_params)
-
-    filter_options = OfferFilterOptions(**filter_params_json)
-    sort_options = OfferSortOptions(**sort_params_json)
+    filter_options = OfferFilterOptions(start_date_time=start_date_time, end_date_time=end_date_time,
+                                        pickup_location=pickup_location, return_location=return_location)
+    sort_options = OfferSortOptions(sort_by_price=sort_by_price)
 
     offers = offer_service.get_all_offers(filter_options, sort_options)
     offers_dict = [offer_to_dict(offer) for offer in offers]
