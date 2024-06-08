8dc26d90e6b6b44c686e6f7fc6c23c37cfe4e6c2
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2024-06-08 16:06:15 +0200

    Request tomtom API for geocoding

--- backend/location/infrastructure/tomtom_geocoding_client.py
+++ backend/location/infrastructure/tomtom_geocoding_client.py
@@ -1,12 +1,22 @@
+import requests
+
 from location.domain.geo_coordinates import GeoCoordinates
 from location.domain.geocoding_client import GeocodingClient
 
 
 class TomTomGeocodingClient(GeocodingClient):
 
+    base_url = "https://api.tomtom.com/search/2/geocode/"
+
     def __init__(self, api_key):
         self.api_key = api_key
 
     def get_coordinates(self, address) -> GeoCoordinates:
-        return GeoCoordinates(40.748817, -73.985428)
+        complete_url = f"{self.base_url}{address}.json"
+        params = {"key": self.api_key}
+        response = requests.get(complete_url, params=params)
+        data = response.json()
+        latitude = data["results"][0]["position"]["lat"]
+        longitude = data["results"][0]["position"]["lon"]
+        return GeoCoordinates(latitude, longitude)
 
